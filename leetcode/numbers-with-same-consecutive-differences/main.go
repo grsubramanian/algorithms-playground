8209d7aae7ac17d6f2bf53a161573fd2d68dc96c
Gokul Ramanan Subramanian <gokul2411s@gmail.com>
2021-06-01 12:38:42 +0100

    leetcode:numbers-with-same-consecutive-differences/v2:Go

--- leetcode/numbers-with-same-consecutive-differences/main.go
+++ leetcode/numbers-with-same-consecutive-differences/main.go
@@ -16,11 +16,9 @@ func numsSameConsecDiff(n int, k int) []int {
         } else {
             multiplier := int(math.Pow(10, float64(n - 1 - pos)))
             for digit := 0; digit <= 9; digit++ {
-                can_try := true
+                var can_try bool
                 if pos == 0 {
-                    if digit == 0 {
-                        continue
-                    }
+                    can_try = digit > 0
                 } else {
                     diff := digit - (work / int(math.Pow(10, float64(n - pos)))) % 10
                     if diff < 0 {
